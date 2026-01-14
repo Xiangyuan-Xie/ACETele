38e38ce4a8ca0a587d10e258eabfc8c8adc384cd
Xiangyuan Xie <dragonboat_xxy@163.com>
2026-01-14 17:27:59 +0800

    refactor(packaging): adapt station refactor and support older Python config loading

--- neutele/config/config_loader.py
+++ neutele/config/config_loader.py
@@ -1,17 +1,19 @@
-import tomllib
 from pathlib import Path
 from typing import Optional, Tuple, Union
 
+import tomli
+
 STATION_MAP = {
-    "flying_hand": ("neutele.station.flying_hand.flying_hand", "FlyingHandStation"),
+    "leader": ("neutele.station.leader.leader", "LeaderStation"),
+    "follower": ("neutele.station.follower.follower", "FollowerStation"),
 }
 
 
 class ConfigLoader:
     def __init__(self, config_path: Optional[Union[str, Path]] = None):
-        config_path = Path(config_path or Path(__file__).parent / "default.toml").expanduser().resolve()
+        config_path = Path(config_path or (Path(__file__).parent / "default.toml")).expanduser().resolve()
         with open(config_path, "rb") as f:
-            self.config = tomllib.load(f)
+            self.config = tomli.load(f)
 
     def get_station_info(self) -> Tuple[str, str]:
         return STATION_MAP[self.config["basic"]["station_type"]]
